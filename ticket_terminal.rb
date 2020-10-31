252b7aad8c2e3ff6069072cd6004aa6a43acae8d
Maedi Prichard <maediprichard@gmail.com>
2020-10-31 11:33:52 +1100

    Add spec.

--- ticket_terminal.rb
+++ ticket_terminal.rb
@@ -7,8 +7,18 @@ class TicketTerminal < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  # Load a list of products from a file.
+
   get '/' do
     'Hello world!'
   end
+
+  # List product details to the user.
+
+  # Add products to a Shopping Cart.
+
+  # Apply promotional discounts.
+
+  # Calculate and display the total cost.
 
 end
