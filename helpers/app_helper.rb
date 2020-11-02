a07e695d6f903d7af2df6f8f79f3de9038bfab07
Maedi Prichard <maediprichard@gmail.com>
2020-11-03 00:14:26 +1100

    Comment AppHelper.

--- helpers/app_helper.rb
+++ helpers/app_helper.rb
@@ -3,26 +3,41 @@ module AppHelper
   # Project path.
   @@root = nil
 
+  ##
   # Expose app to components.
+  #
+  # @param app [App] The main Sinatra::Base instance.
+  ##
   def self.set_app(app)
     @@app ||= app
   end
 
+  ##
   # Expose session to components.
+  #
+  # @param session [Session] The server session.
+  ##
   def self.set_session(session)
     @@session ||= session
   end
 
+  ##
+  # Load database from JSON file.
+  #
+  # @param root [String] Absolute path to the root of the project.
+  # @return [Hash] The database.
+  ##
   def self.load_db(root)
     @@db ||= begin
 
+      # Get data.
       tickets_file = File.read(File.join(root, 'data/tickets.json'))
       tickets_data = JSON.parse(tickets_file)
 
+      # Build database.
       db = {
         :tickets => {}
       }
-
       tickets_data.each do |ticket|
         db[:tickets][ticket['uuid']] = {
           :name => ticket['name'],
@@ -30,7 +45,9 @@ module AppHelper
         }
       end
 
+      # Return database.
       db
+
     end
   end
 
